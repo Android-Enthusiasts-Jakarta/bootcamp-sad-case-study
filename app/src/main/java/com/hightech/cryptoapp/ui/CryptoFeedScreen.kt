ffc62d728eecb4a65c9e3e5f4b1b69a66d54d44e
Syntax <fiqrihafzainislami.com>
2023-07-07 09:37:47 +0700

    Create `MainActivity` class

--- app/src/main/java/com/hightech/cryptoapp/ui/CryptoFeedScreen.kt
+++ app/src/main/java/com/hightech/cryptoapp/ui/CryptoFeedScreen.kt
@@ -1,7 +1,31 @@
 package com.hightech.cryptoapp.ui
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.hightech.cryptoapp.theme.CryptoAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CryptoAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                ) {
+                    CryptoFeedRoute()
+                }
+            }
+        }
+    }
+}
 
 @Composable
 fun CryptoFeedRoute() {
