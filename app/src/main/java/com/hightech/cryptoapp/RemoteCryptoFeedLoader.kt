6c85cb7564178fce3380e92244d8b74e710cf449
Syntax <fiqrihafzainislami.com>
2023-07-07 18:07:29 +0700

    Delivers connectivity error on client error

--- app/src/main/java/com/hightech/cryptoapp/RemoteCryptoFeedLoader.kt
+++ app/src/main/java/com/hightech/cryptoapp/RemoteCryptoFeedLoader.kt
@@ -1,4 +1,34 @@
 package com.hightech.cryptoapp
 
-class RemoteCryptoFeedLoader {
+import com.hightech.cryptoapp.domain.CryptoFeedResult
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import java.io.IOException
+
+sealed class HttpClientResult {
+    data class Failure(val throwable: Throwable) : HttpClientResult()
 }
+
+class CryptoFeedRetrofitHttpClient {
+     fun get(): Flow<HttpClientResult> = flow {
+        try { } catch (throwable: Throwable) {
+            if (throwable is IOException) {
+                emit(HttpClientResult.Failure(Connectivity()))
+            }
+        }
+    }.flowOn(Dispatchers.IO)
+}
+
+class RemoteCryptoFeedLoader constructor(private val httpClient: CryptoFeedRetrofitHttpClient) {
+    fun load(): Flow<CryptoFeedResult> = flow {
+        httpClient.get().collect { result ->
+            if (result is HttpClientResult.Failure) {
+                emit(CryptoFeedResult.Failure(Connectivity()))
+            }
+        }
+    }
+}
+
+class Connectivity : Throwable()
