f817b3a46d800828798acc24e2c50de55019e823
Syntax <fiqrihafzainislami.com>
2023-07-07 11:41:49 +0700

    Create `CryptoFeedItem` model

---
+++ app/src/main/java/com/hightech/cryptoapp/CryptoFeed.kt
@@ -0,0 +1,22 @@
+package com.hightech.cryptoapp
+
+data class CryptoFeedItem(
+    val coinInfo: CoinInfoItem,
+    val raw: RawItem,
+)
+
+data class CoinInfoItem(
+    val id: String,
+    val name: String,
+    val fullName: String,
+    val imageUrl: String
+)
+
+data class RawItem(
+    val usd: UsdItem
+)
+
+data class UsdItem(
+    val price: Double,
+    val changePctDay: Float
+)
