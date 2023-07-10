9ba507e75fb50580dfadb073c31be94b967caa54
Syntax <fiqrihafzainislami.com>
2023-07-11 06:38:27 +0700

    Delivers invalid data with an error in the request, resulting in HTTP 422 responses

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,6 @@ dependencies {
     androidTestImplementation(libs.ui.test.junit4)
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
+
+    implementation(libs.retrofit)
 }
--- app/src/main/java/com/hightech/cryptoapp/CryptoFeedRetrofitHttpClient.kt
+++ app/src/main/java/com/hightech/cryptoapp/CryptoFeedRetrofitHttpClient.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
+import retrofit2.HttpException
 import java.io.IOException
 
 class CryptoFeedRetrofitHttpClient: HttpClient {
@@ -11,6 +12,12 @@ class CryptoFeedRetrofitHttpClient: HttpClient {
         try { } catch (throwable: Throwable) {
             if (throwable is IOException) {
                 emit(HttpClientResult.Failure(Connectivity()))
+            }
+
+            if (throwable is HttpException) {
+                if (throwable.code() == 422) {
+                    HttpClientResult.Failure(InvalidData())
+                }
             }
         }
     }.flowOn(Dispatchers.IO)
