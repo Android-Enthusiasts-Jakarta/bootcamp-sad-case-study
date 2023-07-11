1624c1ec13c59b692efc54c3c3752545e31bbceb
Syntax <fiqrihafzainislami.com>
2023-07-11 19:51:48 +0700

    Load crypto feed automatically when view is presented

--- app/src/main/java/com/hightech/cryptoapp/domain/CryptoFeedLoader.kt
+++ app/src/main/java/com/hightech/cryptoapp/domain/CryptoFeedLoader.kt
@@ -1,12 +1,47 @@
 package com.hightech.cryptoapp.domain
 
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.hightech.cryptoapp.main.factories.RemoteCryptoFeedLoaderFactory
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 
 sealed class CryptoFeedResult {
-    data class Success(val cryptoFeedItems: List<CryptoFeedItem>) : CryptoFeedResult()
+    data class Success(val cryptoFeedItems: List<CryptoFeedItem>?) : CryptoFeedResult()
     data class Failure(val throwable: Throwable) : CryptoFeedResult()
 }
 
 interface CryptoFeedLoader {
     fun load(): Flow<CryptoFeedResult>
+}
+
+class CryptoFeedViewModel constructor(
+    private val cryptoFeedLoader: CryptoFeedLoader
+) : ViewModel() {
+
+    init {
+        loadCryptoFeed()
+    }
+
+    private fun loadCryptoFeed() {
+        viewModelScope.launch {
+            cryptoFeedLoader.load().collect { result ->
+                Log.d("CryptoFeedViewModel", "loadCryptoFeed execute $result")
+            }
+        }
+    }
+
+    companion object {
+        val FACTORY: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                CryptoFeedViewModel(
+                    RemoteCryptoFeedLoaderFactory.createRemoteCryptoFeedLoader()
+                )
+            }
+        }
+    }
 }
--- app/src/main/java/com/hightech/cryptoapp/ui/CryptoFeedScreen.kt
+++ app/src/main/java/com/hightech/cryptoapp/ui/CryptoFeedScreen.kt
@@ -1,6 +1,7 @@
 package com.hightech.cryptoapp.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,7 +9,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.hightech.cryptoapp.domain.CryptoFeedViewModel
 import com.hightech.cryptoapp.theme.CryptoAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,7 +33,9 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CryptoFeedRoute() {
+fun CryptoFeedRoute(
+    viewModel: CryptoFeedViewModel = viewModel(factory = CryptoFeedViewModel.FACTORY)
+) {
     CryptoFeedScreen()
 }
 
