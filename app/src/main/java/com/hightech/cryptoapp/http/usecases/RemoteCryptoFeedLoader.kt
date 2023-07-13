e47c3769f7cd23b4106590fe54abc9715ce828ae
Syntax <fiqrihafzainislami.com>
2023-07-13 15:51:51 +0700

    Refactor to simplify result handling

--- app/src/main/java/com/hightech/cryptoapp/http/usecases/RemoteCryptoFeedLoader.kt
+++ app/src/main/java/com/hightech/cryptoapp/http/usecases/RemoteCryptoFeedLoader.kt
@@ -1,12 +1,11 @@
 package com.hightech.cryptoapp.http.usecases
 
+import android.util.Log
 import com.hightech.cryptoapp.domain.CryptoFeedItemsMapper
 import com.hightech.cryptoapp.domain.CryptoFeedLoader
 import com.hightech.cryptoapp.domain.CryptoFeedResult
-import com.hightech.cryptoapp.http.ConnectivityException
 import com.hightech.cryptoapp.http.CryptoFeedHttpClient
 import com.hightech.cryptoapp.http.HttpClientResult
-import com.hightech.cryptoapp.http.InvalidDataException
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
@@ -14,22 +13,26 @@ class RemoteCryptoFeedLoader constructor(private val cryptoFeedHttpClient: Crypt
     CryptoFeedLoader {
     override fun load(): Flow<CryptoFeedResult> = flow {
         cryptoFeedHttpClient.get().collect { result ->
-            if (result is HttpClientResult.Success) {
-                val cryptoFeed = result.root.data
-                if (cryptoFeed.isNotEmpty()) {
-                    emit(CryptoFeedResult.Success(CryptoFeedItemsMapper.map(cryptoFeed)))
-                } else {
-                    emit(CryptoFeedResult.Success(emptyList()))
-                }
-            }
-
-            if (result is HttpClientResult.Failure) {
-                if (result.throwable is InvalidDataException) {
-                    emit(CryptoFeedResult.Failure(InvalidData()))
+            when (result) {
+                is HttpClientResult.Success -> {
+                    val cryptoFeed = result.root.data
+                    if (cryptoFeed.isNotEmpty()) {
+                        emit(CryptoFeedResult.Success(CryptoFeedItemsMapper.map(cryptoFeed)))
+                    } else {
+                        emit(CryptoFeedResult.Success(emptyList()))
+                    }
                 }
 
-                if (result.throwable is ConnectivityException) {
-                    emit(CryptoFeedResult.Failure(Connectivity()))
+                is HttpClientResult.Failure -> {
+                    when (result.throwable) {
+                        is Connectivity -> {
+                            emit(CryptoFeedResult.Failure(Connectivity()))
+                        }
+
+                        is InvalidData -> {
+                            emit(CryptoFeedResult.Failure(InvalidData()))
+                        }
+                    }
                 }
             }
         }
