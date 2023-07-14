e6ea0035484894694604336edc884a7e4e46611a
Syntax <fiqrihafzainislami.com>
2023-07-14 15:50:11 +0700

    Provide `RemoteCryptoFeedLoaderFactory` use dagger

--- app/src/main/java/com/hightech/cryptoapp/main/factories/RemoteCryptoFeedLoaderFactory.kt
+++ app/src/main/java/com/hightech/cryptoapp/main/factories/RemoteCryptoFeedLoaderFactory.kt
@@ -2,13 +2,13 @@ package com.hightech.cryptoapp.main.factories
 
 import com.hightech.cryptoapp.crypto.feed.domain.CryptoFeedLoader
 import com.hightech.cryptoapp.crypto.feed.http.usecases.RemoteCryptoFeedLoader
+import dagger.Binds
+import dagger.Module
 
-class RemoteCryptoFeedLoaderFactory {
-    companion object {
-        fun createRemoteCryptoFeedLoader(): CryptoFeedLoader {
-            return RemoteCryptoFeedLoader(
-                CryptoFeedHttpClientFactory.createCryptoFeedHttpClient()
-            )
-        }
-    }
+@Module
+abstract class RemoteCryptoFeedLoaderFactory {
+    @Binds
+    abstract fun createRemoteCryptoFeedLoader(
+        remoteCryptoFeedLoader: RemoteCryptoFeedLoader
+    ): CryptoFeedLoader
 }
