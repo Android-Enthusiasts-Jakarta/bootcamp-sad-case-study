e1917e7d34c4a87df9cb4009dd4157282a871be2
Syntax <fiqrihafzainislami.com>
2023-07-14 07:29:10 +0700

    Move `MainActivity` to its own file

---
+++ app/src/main/java/com/hightech/cryptoapp/main/factories/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.hightech.cryptoapp.main.factories
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.hightech.cryptoapp.theme.CryptoAppTheme
+import com.hightech.cryptoapp.ui.CryptoFeedRoute
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CryptoAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                ) {
+                    CryptoFeedRoute()
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/hightech/cryptoapp/ui/CryptoFeedScreen.kt
+++ app/src/main/java/com/hightech/cryptoapp/ui/CryptoFeedScreen.kt
@@ -1,9 +1,6 @@
 package com.hightech.cryptoapp.ui
 
-import android.os.Bundle
 import android.util.Log
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -15,9 +12,7 @@ import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -29,25 +24,8 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.hightech.cryptoapp.presentation.CryptoFeedUiState
 import com.hightech.cryptoapp.presentation.CryptoFeedViewModel
-import com.hightech.cryptoapp.theme.CryptoAppTheme
 import com.hightech.cryptoapp.theme.Purple40
 import com.hightech.cryptoapp.ui.components.CryptoFeedList
-
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            CryptoAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
-                ) {
-                    CryptoFeedRoute()
-                }
-            }
-        }
-    }
-}
 
 @Composable
 fun CryptoFeedRoute(
