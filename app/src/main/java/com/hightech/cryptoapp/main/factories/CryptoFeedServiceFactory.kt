5724860a2d3a76bea1b8b3ad8f4a6ab276645bf3
Syntax <fiqrihafzainislami.com>
2023-07-14 15:49:45 +0700

    Provide `CryptoFeedServiceFactory` use dagger

--- app/src/main/java/com/hightech/cryptoapp/main/factories/CryptoFeedServiceFactory.kt
+++ app/src/main/java/com/hightech/cryptoapp/main/factories/CryptoFeedServiceFactory.kt
@@ -1,17 +1,14 @@
 package com.hightech.cryptoapp.main.factories
 
-import com.hightech.cryptoapp.frameworks.HttpFactory
 import com.hightech.cryptoapp.crypto.feed.http.CryptoFeedService
+import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
 
+@Module
 class CryptoFeedServiceFactory {
-    companion object {
-        fun createCryptoFeedService(): CryptoFeedService {
-            return HttpFactory.createRetrofit(
-                HttpFactory.createMoshi(),
-                HttpFactory.createOkhttpClient(
-                    HttpFactory.createLoggingInterceptor()
-                )
-            ).create(CryptoFeedService::class.java)
-        }
+    @Provides
+    fun createCryptoFeedService(retrofit: Retrofit): CryptoFeedService {
+        return retrofit.create(CryptoFeedService::class.java)
     }
 }
