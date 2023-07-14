a6e6a0afb4f6f1c14f330fbf4ae18c44763a59dd
Syntax <fiqrihafzainislami.com>
2023-07-14 15:54:20 +0700

    Add all modules to the graph

--- app/src/main/java/com/hightech/cryptoapp/main/factories/MainComponent.kt
+++ app/src/main/java/com/hightech/cryptoapp/main/factories/MainComponent.kt
@@ -1,13 +1,24 @@
 package com.hightech.cryptoapp.main.factories
 
+import com.hightech.cryptoapp.crypto.feed.presentation.CryptoFeedViewModel
+import com.hightech.cryptoapp.frameworks.HttpFactory
 import dagger.Component
 import javax.inject.Singleton
 
-@Component
+@Component(
+    modules = [
+        RemoteCryptoFeedLoaderFactory::class,
+        CryptoFeedHttpClientFactory::class,
+        CryptoFeedServiceFactory::class,
+        HttpFactory::class
+    ]
+)
 @Singleton
 interface MainComponent {
     @Component.Factory
     interface Factory {
         fun create(): MainComponent
     }
+
+     fun cryptoFeedViewModel(): CryptoFeedViewModel
 }
