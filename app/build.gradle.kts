2b4a29a917ca581adb6cb41dbdb06fcd8c17ac83
Syntax <fiqrihafzainislami.com>
2023-07-14 14:01:48 +0700

    Add dagger dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    kotlin("kapt")
 }
 
 android {
@@ -27,11 +28,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -75,4 +76,7 @@ dependencies {
     implementation(libs.converter.moshi)
 
     implementation(libs.coil)
+
+    implementation(libs.dagger.android)
+    kapt(libs.dagger.compiler)
 }
